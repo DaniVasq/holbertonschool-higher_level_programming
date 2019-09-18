0d31ed33d7b559043a977c7f1c7b95d8ba2836ba
DaniVasq <danivasq50@gmail.com>
2019-09-18 04:24:04 +0000

    Uploading task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+    counter = 0
+    for y in x:
+        if counter == len(x) - 1:
+          print("{:d}".format(y), end=" ")
+        else:
+         print("{:d}".format(y), end=" ")
+        i += 1
+    print()
