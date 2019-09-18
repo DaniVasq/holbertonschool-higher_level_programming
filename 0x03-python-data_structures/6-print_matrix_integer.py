777e4fba4cb7cf00296b7beb7f3ba41c5faff6e5
DaniVasq <danivasq50@gmail.com>
2019-09-18 04:28:32 +0000

    Fixing indentation

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
-    counter = 0
-    for y in x:
-        if counter == len(x) - 1:
-          print("{:d}".format(y), end=" ")
-        else:
-         print("{:d}".format(y), end=" ")
-        counter += 1
-    print()
+        counter = 0
+        for y in x:
+            if counter == len(x) - 1:
+                print("{:d}".format(y), end=" ")
+            else:
+                print("{:d}".format(y), end=" ")
+            counter += 1
+        print()
