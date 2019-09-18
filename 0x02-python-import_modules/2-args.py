615b6f94bb78a6bd49ecab99d917cc1f7938b271
DaniVasq <danivasq50@gmail.com>
2019-09-18 00:43:20 +0000

    Uploading task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("{}: {}".format(1, sys.argv[1]))
+    else:
+        print("{} arguments:".format.(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv), 1):
+            print("{}: {}".format(i, sys.argv[i]))
