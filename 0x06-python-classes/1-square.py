01b7cb4097414813b6dd137ea5067bdb029edc3c
DaniVasq <danivasq50@gmail.com>
2019-10-01 23:46:46 +0000

    Uploading task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """class Square"""
+    def __init__(self, size):
+        """instantiates with size"""
+        self.__size = size
+        """assigns size as private instance attribute"""
