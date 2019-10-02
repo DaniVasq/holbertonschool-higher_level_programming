cc47aa8f0459e203ded90dc43818815996c4d882
DaniVasq <danivasq50@gmail.com>
2019-10-02 00:07:14 +0000

    Uploading task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """class Square"""
+    def __init__(self, size=0):
+        """init size as zero, an int"""
+        self.__size = size
+        """assigning size as a private instance attribute"""
+        if not isinstance(size, int):
+            """if not of type int, then..."""
+            raise TypeError("size must be an integer")
+        if size <= 0:
+            raise ValueError("size must be >= 0")
+            """raise exceptions"""
