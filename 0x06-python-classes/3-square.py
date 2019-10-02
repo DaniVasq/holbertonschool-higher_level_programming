779964dc376f0371b5c39f86794e83f21350f2aa
DaniVasq <danivasq50@gmail.com>
2019-10-02 00:34:33 +0000

    uploading task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """a class Square"""
+    def __init__(self, size=0):
+        """init size with int zero"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+    @property
+    def area(self):
+        return self.__size * self.__size
+        """returns the area of square"""
