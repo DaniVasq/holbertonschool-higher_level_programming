b4d807e6cbb1e439c8de4955bd8fa0d0f007a7a4
DaniVasq <danivasq50@gmail.com>
2019-10-02 01:24:24 +0000

    Deleting error code

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -24,7 +24,7 @@
         elif value < 0:
             raise ValueError("size must be >= 0")
             """raise exceptions"""
-        return self.__size = value
+        self.__size = value
     def area(self):
         return self.__size * self.__size
         """returns area of square"""
