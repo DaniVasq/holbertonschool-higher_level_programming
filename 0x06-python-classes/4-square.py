4c3e9de9d00f931fef86339d51b0cbc4c631ecb5
DaniVasq <danivasq50@gmail.com>
2019-10-02 01:22:26 +0000

    Fixing code

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
-class Square:
+ class Square:
     """class Square"""
     def __init__(self, size=0):
         """init size as zero, an int"""
         self.__size = size
+        """assigning size as a private instance attribute"""
+        if not isinstance(size, int):
+            """if not of type int, then..."""
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+            """raise exceptions"""
     @property
     def size(self):
-        """getter to retrieve size"""
+        """getter to access private size"""
         return self.__size
     @size.setter
     def size(self, value):
         """setter to set value of size"""
         if not isinstance(value, int):
+            """if not of type int, then..."""
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+            """raise exceptions"""
+        return self.__size = value
     def area(self):
         return self.__size * self.__size
         """returns area of square"""
