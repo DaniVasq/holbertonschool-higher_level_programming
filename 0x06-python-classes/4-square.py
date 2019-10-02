345bd47a5684aafa22ba28e38d1ac201954724be
DaniVasq <danivasq50@gmail.com>
2019-10-02 01:15:17 +0000

    Fixing logic

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,17 +4,17 @@ class Square:
     def __init__(self, size=0):
         """init size as zero, an int"""
         self.__size = size
-        if not isinstance(size, int):
-            """if not of type int, then..."""
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-            """raise exceptions"""
+    @property
     def size(self):
         """getter to retrieve size"""
         return self.__size
+    @size.setter
     def size(self, value):
         """setter to set value of size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
     def area(self):
         return self.__size * self.__size
