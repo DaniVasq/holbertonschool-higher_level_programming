8b39114c4621e8b4345a3ae71fbe3b29773bf025
DaniVasq <danivasq50@gmail.com>
2019-10-02 00:40:50 +0000

    Fixing code, forgot to assign private method

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,6 +3,8 @@ class Square:
     """a class Square"""
     def __init__(self, size=0):
         """init size with int zero"""
+        self.__size = size
+        """assigning private"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
