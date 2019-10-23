0968851afde50df7f739ba124790a0f3c907bbbf
DaniVasq <danivasq50@gmail.com>
2019-10-23 04:15:43 +0000

    Adding task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -99,3 +99,9 @@ class Rectangle(Base):
             for c in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """overriding the string method to return output"""
+
+        return ("[Rectangle] ({}) {}/{} - {}/{}").
+        format(self.id, self.x, self.y, self.width, self.height)
