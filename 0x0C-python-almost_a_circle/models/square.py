9694fe34fb616bde60e2fa3be4161fea67e61fa0
DaniVasq <danivasq50@gmail.com>
2019-10-23 04:40:03 +0000

    Fixing indentation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """Square class module"""
-
-from models.base import Base
 from models.rectangle import Rectangle
+
 
 class Square(Rectangle):
     """class Square inheriting from Rectangle class"""
 
     def __init__(self, size, x=0, y=0, id=None):
         """instantiation of class Square"""
+
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """str method that returns output """
+        """str method that returns output"""
 
         return ("[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width))
