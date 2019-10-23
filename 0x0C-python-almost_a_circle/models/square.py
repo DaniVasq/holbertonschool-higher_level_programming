6976041fb85a3387c005d9c1e926cb47b14b74cb
DaniVasq <danivasq50@gmail.com>
2019-10-23 05:19:55 +0000

    Dictionary rep

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,6 +32,7 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs)
         """assigns attributes"""
+
         if args is not None and len(args) != 0:
             for i, j in enumerate(args):
                 if i == 0:
@@ -45,3 +46,18 @@ class Square(Rectangle):
         else:
             for c, j in kwargs.items():
                 setattr(self, c, j)
+
+    def to_dictionary(self):
+        """dictionary rep"""
+
+        my_dict = {'id': 0, 'size': 0, 'x': 0, 'y': 0}
+        for i in my_dict:
+            if i == 'id':
+                my_dict[i] = self.id
+            if i == 'size':
+                my_dict[i] = self.width
+            if i == 'x':
+                my_dict[i] = self.x
+            if i == 'y':
+                my_dict[i] = self.y
+        return(my_dict)
