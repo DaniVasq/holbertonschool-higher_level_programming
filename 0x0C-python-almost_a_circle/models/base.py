32af69cbf3d50f3f9a956d6fc9e385179c345890
DaniVasq <danivasq50@gmail.com>
2019-10-24 23:26:21 +0000

    Uploading turtle code

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,33 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+@staticmethod
+    def draw(list_rectangles, list_squares):
+        dani = turtle.Turtle()
+        dani.shape("turtle")
+        dani.color("Dark Salmon")
+        for r in list_rectangles:
+            dani.showturtle()
+            dani.up()
+            dani.goto(r.x, r.y)
+            dani.down()
+            for i in range(2):
+                dani.forward(r.width)
+                dani.left(90)
+                dani.forward(r.height)
+                dani.left(90)
+            dani.hideturtle()
+        dani.color("Medium Aquamarine")
+        for s in list_squares:
+            dani.showturtle()
+            dani.up()
+            dani.goto(s.x, s.y)
+            dani.down()
+            for i in range(2):
+                dani.forward(s.width)
+                dani.left(90)
+                dani.forward(s.width)
+                dani.left(90)
+            dani.hideturtle()
+        dani.done()
