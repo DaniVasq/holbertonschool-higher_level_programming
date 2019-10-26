4ce75a85ce35541c5583b4ef973422c3cd08a9d8
DaniVasq <danivasq50@gmail.com>
2019-10-26 05:22:41 +0000

    Adding task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -66,3 +66,12 @@ class Base:
             else:
                 f.write(cls.to_json_string(
                     [(cls.to_dictionary(obj)) for obj in list_objs]))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns list of JSON str rep of json_string"""
+
+        if json_string is None or len(json_string) == []:
+            return "[]"
+        else:
+            return json_string
