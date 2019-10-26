8409b7bb641261fd41a3c284fd013174b714ea1b
DaniVasq <danivasq50@gmail.com>
2019-10-26 04:40:04 +0000

    Uploading all square methds

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,36 @@ class Square(Rectangle):
 
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """update method"""
+        j = 0
+        if isinstance(args, tuple) and len(args) > 0:
+            for arg in args:
+                if j == 0:
+                    self.id = arg
+                if j == 1:
+                    self.size = arg
+                if j == 2:
+                    self.x = arg
+                if j == 3:
+                    self.y = arg
+                j += 1
+        for k, v in kwargs.items():
+            if k == "id":
+                self.id = v
+            if k == "size":
+                self.size = v
+            if k == "x":
+                self.x = v
+            if k == "y":
+                self.y = v
+
+    def to_dictionary(self):
+        """return the dict representation of a square"""
+        my_dict = {}
+        my_dict["id"] = self.id
+        my_dict["size"] = self.size
+        my_dict["x"] = self.x
+        my_dict["y"] = self.y
+        return my_dict
