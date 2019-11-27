17553c083bd140c29ae8bc3375d8de72d0c4c169
DaniVasq <danivasq50@gmail.com>
2019-11-27 02:05:11 +0000

    Adding task 1

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,2 @@
 -- Script that lists all privileges of two users on my server.
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost'
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1', 'user_0d_2'@'localhost';
