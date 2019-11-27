e6189f1612a0b5fd74da6e6282c3d28c6675477c
DaniVasq <danivasq50@gmail.com>
2019-11-27 03:03:33 +0000

    Task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- Script that create a table with a description.
-CREATE TABLE IF NOT EXISTS id_not_null (id INT=1, name VARCHAR(256);
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256);
