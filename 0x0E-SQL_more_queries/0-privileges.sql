46bb71070fa68bfc55b1796b03608adcb72d8806
DaniVasq <danivasq50@gmail.com>
2019-11-27 02:12:57 +0000

    Fixing script

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- Script that lists all privileges of two users on my server.
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1', 'user_0d_2'@'localhost';
+SHOW GRANTS user_0d_1;
+SHOW GRANTS user_0d_2;
