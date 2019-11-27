dc679562382b5cf8c96a55a93f6f5ff00f3a436b
DaniVasq <danivasq50@gmail.com>
2019-11-27 02:19:33 +0000

    Fixing syntax

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Script that lists all privileges of two users on my server.
-SHOW GRANTS for user_0d_1@localhost;
-SHOW GRANTS for user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
