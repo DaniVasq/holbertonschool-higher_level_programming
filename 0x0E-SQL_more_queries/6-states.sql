b3268e48bd47852377d46aa685f130d25cd1b3b3
DaniVasq <danivasq50@gmail.com>
2019-11-27 03:47:14 +0000

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- creates a database and table with descriptions and conditions
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       name VARCHAR(256) NOT NULL
+);
