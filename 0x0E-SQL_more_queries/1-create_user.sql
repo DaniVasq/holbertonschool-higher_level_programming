10f5b3a38c8d094a174781685aab753f38f036be
DaniVasq <danivasq50@gmail.com>
2019-11-27 02:23:58 +0000

    Adding task 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Script to create user with privileges and password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'USER_0d_1'@'localhost';
