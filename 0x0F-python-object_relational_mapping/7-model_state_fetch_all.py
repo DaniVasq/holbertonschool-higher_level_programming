071f154aa69e8637103f2a0f14170a3501b90bae
DaniVasq <danivasq50@gmail.com>
2019-12-15 02:22:24 +0000

    Adding task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,8 +2,8 @@
 """Start link class to table in database 
 """
 import sys
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import State
 
 if __name__ == "__main__":
