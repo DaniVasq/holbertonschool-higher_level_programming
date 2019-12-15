eb4455b23f4c5e16ac7bff2dd2193db0a3c761fd
DaniVasq <danivasq50@gmail.com>
2019-12-15 03:13:20 +0000

    Adding task 14

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Base model """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class City(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey('states.id')
